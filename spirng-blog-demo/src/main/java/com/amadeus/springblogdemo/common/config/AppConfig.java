ee345fa7619de31a2f8ec908f127b9fd6a97233e
mulaihongliqi627-lgtm <mulaihongliqi627@gmail.com>
2026-05-27 20:41:36 +0800

    修改前

--- spirng-blog-demo/src/main/java/com/amadeus/springblogdemo/common/config/AppConfig.java
+++ spirng-blog-demo/src/main/java/com/amadeus/springblogdemo/common/config/AppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
-public class AppCongig implements WebMvcConfigurer {
+public class AppConfig implements WebMvcConfigurer {
 
     @Autowired
     private LoginInterceptor loginInterceptor;
